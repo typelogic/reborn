4fdcd0aed33f53e88e2539c2743207156e5ce38c
truth <truthadjustr@gmail.com>
2019-07-31 22:02:15 +0800

    starting...

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+#if 0
+#include "include/gtest/gtest.h"
+
 void print(std::string);
+
+class Fixture : public testing::Test
+{
+    public:
+    void SetUp() override
+    {
+        Poco::TemporaryFile tmp;
+        mTestHomeFolder = tmp.tempName();
+        Poco::File x(mTestHomeFolder);
+        x.createDirectories();
+    }
+
+    void TearDown() override
+    {
+        Poco::File x(mTestHomeFolder);
+        x.remove(true);
+    }
+
+    std::string mTestHomeFolder;
+};
+#endif 
 
 int main (int argc, char *argv[])
 {
