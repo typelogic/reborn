4d6af4cb47f5adafcb2dd14e8077150963ce8a8a
truth <truthadjustr@gmail.com>
2019-07-31 12:51:45 +0800

    Prefered is std::make_shared

--- main.cpp
+++ main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <memory>
 
 void print(std::string);
 
+class Dog {
+    public: 
+    void bark() {
+        print("bark");
+    }
+
+    ~Dog() {
+        print("~Dog");
+    }
+};
+
 int main (int argc, char *argv[])
 {
+#if 0
+    Dog* p = new Dog();
+    delete p;
+
+    p->bark();
+#else
+    std::shared_ptr<Dog> p(new Dog());
+
+    {
+        #if 0
+        // bad wrong way, it destroys x
+        Dog* x = new Dog();
+        std::shared_ptr<Dog> obj1(x); // obj1.use_count() == 1
+        std::shared_ptr<Dog> obj2(x);
+        #endif
+
+
+        std::shared_ptr<Dog> aaa = std::make_shared<Dog>(); // faster and safer
+
+        std::shared_ptr<Dog> q = p;
+        q->bark();
+
+        print(std::to_string(q.use_count())); // prints 2
+    }
+
+    p->bark();
+#endif
     return 0;
 }
